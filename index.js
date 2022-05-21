2f665f754f2648ac724dd1bf3e128b99e3a4a1d1
Jason Rothgeb <jrothgeb@gmail.com>
2022-05-21 12:28:58 -0500

    commit tree 5a624d1e18a1e7472e9452069f70b125ee5828f3

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function appendCat(name) {
+    const catAddNameEnd = [...cats,name]
+    return catAddNameEnd;
+    //const allCats = [...cats,console.log(name)]
+}
+function prependCat(name) {
+    const catAddNameBegin = [name,...cats];
+    return catAddNameBegin;
+}
+function removeLastCat(name) {
+    const noLastCat = cats.slice(0,2);
+    return noLastCat;
+}
+function removeFirstCat(name) {
+    const noFirstCat = cats.slice(1);
+    return noFirstCat;
+}
+
+
+
